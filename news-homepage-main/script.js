3b2327756fa00762611f7669f95dd5741908fd50
braxvalle <braxvalle2@gmail.com>
2023-01-23 17:56:41 +0300

    news

---
+++ news-homepage-main/script.js
@@ -0,0 +1,9 @@
+
+function toggleMenu() {
+    var nav = document.getElementsByClassName(".active");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
